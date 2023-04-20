3daeafdf4007c9b68cd6a3824987fa295abfd350
Aremu Olakunle Umaru <colakunleumaru@gmail.com>
2023-04-20 04:07:39 -0700

    did some things again

--- client/src/components/SingleEvent/SingleEvent.jsx
+++ client/src/components/SingleEvent/SingleEvent.jsx
@@ -1,15 +1,13 @@
 import "./SingleEvent.css";
-import {Link} from "react-router-dom"
+
 const SingleEvent=({_id,image,name,venue,address,date,time})=>{
     return(
         <div className="SingleEvent">
-            <Link to={`/event/${_id}`}>
-                <img src={image} alt="Event" />
-                <span>{name}</span>
-                <span>{venue}-{address}</span>
-                <span>{time}</span>
-                <span>{date}</span>
-            </Link>
+            <img src={image} alt="Event" />
+            <span>{name}</span>
+            <span>{venue}-{address}</span>
+            <span>{time}</span>
+            <span>{date}</span>
         </div>        
     )
 }
